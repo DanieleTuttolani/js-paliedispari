586f6fd0546ed670a0106c5e0cfbb02d025f9df9
DanieleTuttolani <danieletuttolani2@gmail.com>
2022-12-16 09:20:12 +0100

    finito

--- script/script.js
+++ script/script.js
@@ -4,20 +4,30 @@ const btn = document.getElementById("submit")
 
 //! functions
 
-function randomNumber(min, max ){
-    const randomNumber = Math.floor(Math.random() * max + min )
+function randomNumber(min = 1, max = 5 ){
+    const randomNumber = Math.floor(Math.random() * max) + min
 
     return randomNumber
 }
 
 btn.addEventListener( "click" , function(){
-    
-    let finalNumber = 
-    userNumber.value + randomNumber( 1, 5)
+    let cpu = randomNumber()
+    let player = parseInt(userNumber.value);
+    let finalNumber = player + cpu;
     console.log(finalNumber)
     
+   console.log( "la scelta è " + userChoice.value)
 
-   if ( userChoice.value === "even"){
+   let numberEven = false
+   let numberUneven = false 
 
-   }
+   if ( finalNumber % 2 ){
+        numberUneven = true
+        console.log( " e dispari")
+   }else{
+       numberEven = true
+       console.log (" è pari")
+    }
+
+    
 })
